6d8c8b22b533c3e81a91a2de156b58789bc818a1
zhupeiwen <zhupeiwen@hyperchain.cn>
2022-11-25 15:18:28 +0800

    fix: add register appchain & rule unit test

--- src/test/java/cn/dmlab/bitxhub/AppchainTest.java
+++ src/test/java/cn/dmlab/bitxhub/AppchainTest.java
@@ -5,7 +5,6 @@ import cn.dmlab.utils.ByteUtil;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
-import org.bouncycastle.util.encoders.Hex;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -21,13 +20,22 @@ public class AppchainTest {
 
     private GrpcClient client;
 
+    private GrpcClient adminClient;
 
-    private Config config = Config.defaultConfig();
+
+    private final Config config = Config.defaultConfig();
+
+    private final Config adminConfig = Config.defaultConfig();
 
     @Before
     public void setUp() {
-        config.setEcKey(ECKeyS256.fromPrivate(ByteUtil.hexStringToBytes("b6477143e17f889263044f6cf463dc37177ac4526c4c39a7a344198457024a2f")));
+        // address: 0xF83625B55Db87bAca3862924777aaCf2E81Cb7eD
+        // transfer token to address first
+        config.setEcKey(ECKeyS256.fromPrivate(ByteUtil.hexStringToBytes("b44b8cc60b96ce7b3ae948d824df7110f1074a0bb5a19ecf24e2314a942f5325")));
         client = new GrpcClientImpl(config);
+
+        adminConfig.setEcKey(ECKeyS256.fromPrivate(ByteUtil.hexStringToBytes("b6477143e17f889263044f6cf463dc37177ac4526c4c39a7a344198457024a2f")));
+        adminClient = new GrpcClientImpl(adminConfig);
     }
 
     @After
@@ -76,7 +84,6 @@ public class AppchainTest {
     }
 
     @Test
-    @Ignore
     public void registerRule() throws IOException {
         byte[] contractBytes = IOUtils.toByteArray(
                 new FileInputStream("target/test-classes/testdata/example.wasm"));
@@ -88,25 +95,51 @@ public class AppchainTest {
         String ret = receipt.getRet().toStringUtf8();
         JSONObject jsonObject = JSONObject.parseObject(ret);
 
-        String appchainID = jsonObject.getString("id");
+        String proposalID = jsonObject.getString("proposal_id");
+
+        ReceiptOuterClass.Receipt voteReceipt = vote(proposalID);
+        Assert.assertNotNull(voteReceipt);
+
+        String appchainID = "did:bitxhub:appchain:.";
 
         ArgOuterClass.Arg[] ruleArgs = Types.toArgArray(
                 Types.string(appchainID),
-                Types.string(contractAddress));
-        ReceiptOuterClass.Receipt ruleReceipt = client.invokeBVMContract(BVMAddr.RULE_MANAGER_CONTRACT_ADDR, "RegisterRule", ruleArgs);
+                Types.string(contractAddress),
+                Types.string("ruleUrl"));
+        ReceiptOuterClass.Receipt ruleReceipt = client.invokeBVMContract(BVMAddr.RULE_MANAGER_CONTRACT_ADDR, "RegisterRuleV2", ruleArgs);
         Assert.assertNotNull(ruleReceipt);
+
+        proposalID = JSONObject.parseObject(ruleReceipt.getRet().toStringUtf8()).getString("proposal_id");
+        voteReceipt = vote(proposalID);
+        Assert.assertNotNull(voteReceipt);
+    }
+
+    ReceiptOuterClass.Receipt vote(String proposalID) {
+        // vote for proposal
+        ArgOuterClass.Arg[] voteArgs = Types.toArgArray(
+                Types.string(proposalID),
+                Types.string("approve"),
+                Types.string("reason"));
+        ReceiptOuterClass.Receipt voteReceipt = adminClient.invokeBVMContract(BVMAddr.GOVERNANCE_CONTRACT_ADDR, "Vote", voteArgs);
+        return voteReceipt;
     }
 
     ReceiptOuterClass.Receipt register() {
         ArgOuterClass.Arg[] args = Types.toArgArray(
+                Types.string("appchain0xF83625B55Db87bAca3862924777aaCf2E81Cb7eD"), //method
+                Types.string("/ipfs/QmQVxzUqN2Yv2UHUQXYwH8dSNkM8ReJ9qPqwJsf8zzoNUi"), //docAddr
+                Types.string("QmQVxzUqN2Yv2UHUQXYwH8dSNkM8ReJ9qPqwJsf8zzoNUi"), //docHash
                 Types.string(""), //validators
-                Types.i32(0), //consensus_type
+                Types.string("rbft"), //consensus_type
                 Types.string("hyperchain"), //chain_type
                 Types.string("税务链"), //name
                 Types.string("趣链税务链"), //desc
                 Types.string("1.8"),//version
-                Types.string("")); //public key
-        ReceiptOuterClass.Receipt receipt = client.invokeBVMContract(BVMAddr.APPCHAIN_MANAGER_CONTRACT_ADDR, "Register", args);
+                Types.string(""), //public key
+                Types.string("reason"), //reason
+                Types.string("0x00000000000000000000000000000000000000a2"), //rule
+                Types.string("")); //rule_url
+        ReceiptOuterClass.Receipt receipt = client.invokeBVMContract(BVMAddr.APPCHAIN_MANAGER_CONTRACT_ADDR, "RegisterV2", args);
         Assert.assertNotNull(receipt);
         return receipt;
     }
